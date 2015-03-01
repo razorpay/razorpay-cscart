845a01e1a80e83e2d58392a31a2ce3778156de0b
Harshil Mathur <harshilmathur@gmail.com>
2015-03-01 00:06:59 -0800

    Updates curl error handling

--- app/payments/razorpay.php
+++ app/payments/razorpay.php
@@ -45,19 +45,31 @@ if (defined('PAYMENT_NOTIFICATION')) {
                     $result = curl_exec($ch);
                     $http_status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 
-                    $array = json_decode($result, true);
 
-                    //close connection
-                    curl_close($ch);
-
-                    //Check success response
-                    if($http_status === 200 and isset($array['error']) === false){
-                        $success = true;    
+                    if($result === false) {
+                        $success = false;
+                        $error = 'Curl error: ' . curl_error($ch);
                     }
                     else {
-                        $error = $array['error']['code'].":".$array['error']['description'];
-                        $success = false;
+                        $response_array = json_decode($result, true);
+                        //Check success response
+                        if($http_status === 200 and isset($response_array['error']) === false){
+                            $success = true;    
+                        }
+                        else {
+                            $success = false;
+
+                            if(!empty($response_array['error']['code'])) {
+                                $error = $response_array['error']['code'].":".$response_array['error']['description'];
+                            }
+                            else {
+                                $error = "RAZORPAY_ERROR:Invalid Response <br/>".$result;
+                            }
+                        }
                     }
+                        
+                    //close connection
+                    curl_close($ch);
                 }
                 catch (Exception $e) {
                     $success = false;
