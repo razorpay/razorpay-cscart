41ef2f306b876cf030988a84e77ebdd517e635c2
Ria Garg <ria.garg@razorpay.com>
2017-03-07 14:44:08 +0530

    Orders API Integration (#3)
    * Orders API Integration
    
    * code refatored
    
    * removed redundant session variable
    
    * code review fixes
    
    * code review fixes
    
    * removed extra lines
    
    * added line break
    
    * added strict checking
    
    * code review fixes
    
    * added window on load
    
    * code review fixes
    
    * code review fixes
    
    * added anchor tag
    
    * quoting the attributes
    
    * added doctype tags

---
+++ app/payments/RazorpayPayment.php
@@ -0,0 +1,171 @@
+<?php
+use Razorpay\Api\Api;
+
+class RazorpayPayment
+{
+    public function getSessionValue($key)
+    {
+        return $_SESSION[$key];
+    }
+
+    public function setSessionValues($sessionValues)
+    {
+        foreach($sessionValues as $key => $value)
+        {
+            $_SESSION[$key] = $value;
+        }
+    }
+
+    public function getOrderData($orderId, $orderInfo, $processorData)
+    {
+        $data = array(
+            'receipt'         => $orderId,
+            'amount'          => fn_rzp_adjust_amount($orderInfo['total'], $processorData['processor_params']['currency'])*100,
+            'currency'        => $processorData['processor_params']['currency'],
+            'payment_capture' => 1
+        );
+
+        return $data;
+    }
+
+    public function generateHtmlForm($url, $json)
+    {
+        $html = <<<EOT
+<!DOCTYPE html>
+<body>
+<script src="https://checkout.razorpay.com/v1/checkout.js"></script>
+<script>
+    var data = $json;
+    data.handler = function (transaction) {
+        document.getElementById('razorpay_payment_id').value = transaction.razorpay_payment_id;
+        document.getElementById('razorpay_signature').value = transaction.razorpay_signature;
+        document.getElementById('razorpay-form').submit();
+    };
+    function razorpaySubmit(){                  
+        var rzp1 = new Razorpay(data);
+        rzp1.open();
+        rzp1.modal.options.backdropClose = false;
+    }
+    window.onload = function() {
+        razorpaySubmit();
+    };
+</script>
+<form name="razorpay-form" id="razorpay-form" action="$url" target="_parent" method="POST">
+    <input type="hidden" name="razorpay_payment_id" id="razorpay_payment_id" />
+    <input type="hidden" name="razorpay_signature" id="razorpay_signature"/>
+</form>
+</body>
+</html>
+EOT;
+
+        return $html;
+    }
+
+    public function processRazorpayResponse()
+    {
+        $razorpaySignature = null;
+        if (isset($_POST['razorpay_signature']) === true)
+        {
+            $razorpaySignature = $_POST['razorpay_signature'];
+        }
+
+        $razorpayPaymentId = null;
+        if (isset($_POST['razorpay_payment_id']) === true)
+        {
+            $razorpayPaymentId = $_POST['razorpay_payment_id'];
+        }
+
+        $merchantOrderId = fn_rzp_place_order($_SESSION['merchant_order_id']);
+
+        $razorpayOrderId = $_SESSION['razorpay_order_id'];
+
+        if ((empty($razorpaySignature) === false) and (empty($razorpayPaymentId) === false))
+        {
+            if (fn_check_payment_script('razorpay.php', $merchantOrderId, $processorData))
+            {
+                $keyId = $processorData['processor_params']['key_id'];
+
+                $keySecret = $processorData['processor_params']['key_secret'];
+
+                $orderInfo = fn_get_order_info($merchantOrderId);
+
+                $amount = fn_rzp_adjust_amount($orderInfo['total'],
+                    $processorData['processor_params']['currency'])*100;
+                    
+                $attributes = array (
+                    'razorpay_signature'  => $razorpaySignature,
+                    'razorpay_order_id'   => $razorpayOrderId,
+                    'razorpay_payment_id' => $razorpayPaymentId
+                );
+
+                $api = new Api($keyId, $keySecret);
+
+                try
+                {
+                    $api->utility->verifyPaymentSignature($attributes);
+                    $success = true;
+                }
+                catch (\Exception $e)
+                {
+                    $success = false;
+                    $error = "PAYMENT_ERROR: Payment failed";
+                }
+            }
+            else
+            {
+                $error = 'RAZORPAY_ERROR: Invalid Response';
+
+                $success = false;    
+            }
+
+            if ($success === true)
+            {
+                $pp_response['order_status'] = 'P';
+                $pp_response['reason_text'] = fn_get_lang_var('text_rzp_success');
+                $pp_response['transaction_id'] = $merchantOrderId;
+                $pp_response['client_id'] = $razorpayPaymentId;
+
+                fn_finish_payment($merchantOrderId, $pp_response);
+                fn_order_placement_routines('route', $merchantOrderId);
+            }
+            else
+            {
+                $pp_response['order_status'] = 'O';
+                $pp_response['reason_text'] = fn_get_lang_var('text_rzp_pending').$error;
+                $pp_response['transaction_id'] = $merchantOrderId;
+                $pp_response['client_id'] = $razorpayPaymentId;
+
+                fn_finish_payment($merchantOrderId, $pp_response);
+                fn_set_notification('E', __('error'), __('text_rzp_failed_order').$merchantOrderId);
+                fn_order_placement_routines('checkout_redirect');
+            }
+        }
+        else
+        {
+            fn_set_notification('E', __('error'), __('text_rzp_failed_order').$merchantOrderId);
+            fn_order_placement_routines('checkout_redirect');
+        }
+    }
+
+    public function getButton()
+    {
+       $url = fn_url("index.php?dispatch=checkout.process_payment&clicked=true", AREA, 'current');
+
+       $html = <<<EOT
+<!DOCTYPE html>
+<body>
+<a href="$url">
+    <button id="mybutton" type="button"
+        style="background-color:#ff5319;height:22px:width:150px;border: none;
+        color: white;font-size: 16px;padding: 6px 7px;">SUBMIT MY ORDER
+    </button>
+</a>
+</body>
+</html>
+EOT;
+
+        return $html;
+    }
+}
+
+?>
--- app/payments/razorpay.php
+++ app/payments/razorpay.php
@@ -1,13 +1,17 @@
 <?php
 use Tygh\Registry;
+use Razorpay\Api\Api;
 
 include_once ('razorpay/razorpay_common.inc');
+require_once ('razorpay-sdk/Razorpay.php');
+require_once('RazorpayPayment.php');
 
 if ( !defined('AREA') ) { die('Access denied'); }
 
 // Return from payment
-if (defined('PAYMENT_NOTIFICATION')) {
-    if ($mode == 'return' && !empty($_REQUEST['merchant_order_id'])) {
+if (defined('PAYMENT_NOTIFICATION'))
+{
+    if ($mode == 'return' && !empty($_POST['razorpay_signature'])) {
         if (isset($view) === false)
         {
             $view = Registry::get('view');
@@ -17,172 +21,77 @@ if (defined('PAYMENT_NOTIFICATION')) {
         $view->display('views/orders/components/placing_order.tpl');
         fn_flush();
 
-        $merchant_order_id = fn_rzp_place_order($_REQUEST['merchant_order_id']);
-        $razorpay_payment_id = $_REQUEST['razorpay_payment_id'];
+        $razorpayPayment = new RazorpayPayment();
 
-        if(!empty($merchant_order_id) and !empty($razorpay_payment_id)){
-            if (fn_check_payment_script('razorpay.php', $merchant_order_id, $processor_data)) {
-                $key_id = $processor_data['processor_params']['key_id'];
-                $key_secret = $processor_data['processor_params']['key_secret'];
-                $order_info = fn_get_order_info($merchant_order_id);
-                $amount = fn_rzp_adjust_amount($order_info['total'], $processor_data['processor_params']['currency'])*100;
-
-                $pp_response = array();
-                $success = false;
-                $error = "";
-
-                try {
-                    $url = 'https://api.razorpay.com/v1/payments/'.$razorpay_payment_id.'/capture';
-                    $fields_string="amount=$amount";
-
-                    //cURL Request
-                    $ch = curl_init();
-
-                    //set the url, number of POST vars, POST data
-                    curl_setopt($ch,CURLOPT_URL, $url);
-                    curl_setopt($ch,CURLOPT_USERPWD, $key_id . ":" . $key_secret);
-                    curl_setopt($ch,CURLOPT_TIMEOUT, 60);
-                    curl_setopt($ch,CURLOPT_POST, 1);
-                    curl_setopt($ch,CURLOPT_POSTFIELDS, $fields_string);
-                    curl_setopt($ch,CURLOPT_RETURNTRANSFER, TRUE);
-
-                    //execute post
-                    $result = curl_exec($ch);
-                    $http_status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-
-
-                    if($result === false) {
-                        $success = false;
-                        $error = 'Curl error: ' . curl_error($ch);
-                    }
-                    else {
-                        $response_array = json_decode($result, true);
-                        //Check success response
-                        if($http_status === 200 and isset($response_array['error']) === false){
-                            $success = true;    
-                        }
-                        else {
-                            $success = false;
-
-                            if(!empty($response_array['error']['code'])) {
-                                $error = $response_array['error']['code'].":".$response_array['error']['description'];
-                            }
-                            else {
-                                $error = "RAZORPAY_ERROR:Invalid Response <br/>".$result;
-                            }
-                        }
-                    }
-                        
-                    //close connection
-                    curl_close($ch);
-                }
-                catch (Exception $e) {
-                    $success = false;
-                    $error ="CSCART_ERROR:Request to Razorpay Failed";
-                }
-
-                if($success === true){
-                    $pp_response['order_status'] = 'P';
-                    $pp_response['reason_text'] = fn_get_lang_var('text_rzp_success');
-                    $pp_response['transaction_id'] = @$order;
-                    $pp_response['client_id'] = $razorpay_payment_id;
-
-                    fn_finish_payment($merchant_order_id, $pp_response);
-                    fn_order_placement_routines('route', $merchant_order_id);
-                }
-                else {
-                    $pp_response['order_status'] = 'O';
-                    $pp_response['reason_text'] = fn_get_lang_var('text_rzp_pending').$error;
-                    $pp_response['transaction_id'] = @$order;
-                    $pp_response['client_id'] = $razorpay_payment_id;
-
-                    fn_finish_payment($merchant_order_id, $pp_response);
-                    fn_set_notification('E', __('error'), __('text_rzp_failed_order').$merchant_order_id);
-                    fn_order_placement_routines('checkout_redirect');
-                }
-
-            }
-        }
-        else {
-            fn_set_notification('E', __('error'), __('text_rzp_failed_order').$_REQUEST['merchant_order_id']);
-            fn_order_placement_routines('checkout_redirect');
-        }
+        $razorpayPayment->processRazorpayResponse();
+        exit;
     }
-    exit;
 }
-else {
-    $url = fn_url("payment_notification.return?payment=razorpay", AREA, 'current');
-    $checkout_url = "https://checkout.razorpay.com/v1/checkout.js";
+else
+{
+    $razorpayPayment = new RazorpayPayment();
 
-    $fields = array(
-        'key' => $processor_data['processor_params']['key_id'],
-        'amount' => fn_rzp_adjust_amount($order_info['total'], $processor_data['processor_params']['currency'])*100,
-        'currency' => $processor_data['processor_params']['currency'],
-        'description' => "Order# ".$order_id,
-        'name' => Registry::get('settings.Company.company_name'),
-        'customer_name' => $order_info['b_firstname']." ".$order_info['b_lastname'],
-        'customer_email' => $order_info['email'],
-        'customer_phone' => $order_info['phone'],
-        'order_id' => $order_id
+    $url = fn_url("payment_notification.return?payment=razorpay", AREA, 'current');
+
+    $data = $razorpayPayment->getOrderData($order_id, $order_info, $processor_data);
+
+    $keyId = $processor_data['processor_params']['key_id'];
+
+    $keySecret = $processor_data['processor_params']['key_secret'];
+
+    $api = new Api($keyId, $keySecret);
+
+    $razorpayOrderId = null;
+
+    try
+    {
+        $razorpayOrder = $api->order->create($data);
+        $razorpayOrderId = $razorpayOrder['id'];
+    }
+    catch (\Exception $e)
+    {
+        echo 'CS Cart Error : ' . $e->getMessage();
+    }
+
+    $sessionValues = array(
+        'razorpay_order_id' => $razorpayOrderId,
+        'merchant_order_id' => $order_id
     );
 
-    $html = '<form name="razorpay-form" id="razorpay-form" action="'.$url.'" target="_parent" method="POST">
-                <input type="hidden" name="razorpay_payment_id" id="razorpay_payment_id" />
-                <input type="hidden" name="merchant_order_id" id="order_id" value="'.$fields['order_id'].'"/>
-            </form>';
-    
-    $js = '<script>';
+    $razorpayPayment->setSessionValues($sessionValues);
 
-    $js .= "var razorpay_options = {
-                'key': '".$fields['key']."',
-                'amount': '".$fields['amount']."',
-                'name': '".$fields['name']."',
-                'description': 'Order# ".$fields['order_id']."',
-                'currency': '".$fields['currency']."',
-                'handler': function (transaction) {
-                    document.getElementById('razorpay_payment_id').value = transaction.razorpay_payment_id;
-                    document.getElementById('razorpay-form').submit();
-                },
-                'prefill': {
-                    'name': '".$fields['customer_name']."',
-                    'email': '".$fields['customer_email']."',
-                    'contact': '".$fields['customer_phone']."'
-                },
-                notes: {
-                    'cs_order_id': '".$fields['order_id']."'
-                },
-                netbanking: true
-            };
-            
-            function razorpaySubmit(){                  
-                var rzp1 = new Razorpay(razorpay_options);
-                rzp1.open();
-                rzp1.modal.options.backdropClose = false;
-            }    
-            
-            var rzp_interval = setInterval(function(){
-                if (typeof window[\"Razorpay\"] != \"undefined\")
-                {
-                    setTimeout(function(){ razorpaySubmit(); }, 500);
-                    clearInterval(rzp_interval);
-                }
-            }, 500);
-            ";
+    $fields = array(
+        'key'         => $keyId,
+        'amount'      => fn_rzp_adjust_amount($order_info['total'], $processor_data['processor_params']['currency'])*100,
+        'currency'    => $processor_data['processor_params']['currency'],
+        'description' => "Order# ".$order_id,
+        'name'        => Registry::get('settings.Company.company_name'),
+        'prefill'     => array(
+            'name'    => $order_info['b_firstname'] . " " . $order_info['b_lastname'],
+            'email'   => $order_info['email'],
+            'contact' => $order_info['phone']
+        ),
+        'notes'       => array(
+            'cs_order_id' => $order_id
+        ),
+        'order_id' => $razorpayOrderId,
+    );
 
-    $js .= '</script>';
-
-    if (!$fields['amount']) {
+    if (!$fields['amount'])
+    {
         echo __('text_unsupported_currency');
         exit;
     }
 
-echo <<<EOT
-    <script src="{$checkout_url}"></script>
-    {$html}
-    {$js}
-</body>
-</html>
-EOT;
+    //checks for iframe mode. In iframe mode payment flow goes through another payment button
+    if ((defined('IFRAME_MODE') === true) and (empty($_GET['clicked']) === true))
+    {
+        echo $razorpayPayment->getButton();
+    }
+    else
+    {
+        echo $razorpayPayment->generateHtmlForm($url, json_encode($fields));
+    }
 exit;
 }
 
